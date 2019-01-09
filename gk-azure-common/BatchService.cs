37d1298e944da0aa4195d1794fb61b993fbb1d1c
Gayathri Rao <Gayathri.Rao@partner.bmw.de>
2019-01-09 11:05:33 +0100

    adding the objects

---
+++ gk-azure-common/BatchService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gk_azure_common
+{
+    public class BatchService
+    {
+        public string GetBatchStatus()
+        {
+            return "SynthesisCompleted";
+        }
+    }
+}
